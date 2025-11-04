e4f45976dad372147c50413d752fbd132f0f53af
SpringIsKTho <sergiodela2006@gmail.com>
2025-11-04 13:26:16 +0100

    Added Intents and Signup Layout

--- app/src/main/java/com/example/nicestart/Signup.java
+++ app/src/main/java/com/example/nicestart/Signup.java
@@ -10,13 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login extends AppCompatActivity {
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -25,12 +25,12 @@ public class Login extends AppCompatActivity {
     }
 
     public void openMain(View v){
-        Intent intent = new Intent(Login.this, MainActivity.class);
+        Intent intent = new Intent(Signup.this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void openSignup(View v){
-        Intent intent = new Intent(Login.this, Signup.class);
+    public void openLogin(View v){
+        Intent intent = new Intent(Signup.this, Login.class);
         startActivity(intent);
     }
 }
