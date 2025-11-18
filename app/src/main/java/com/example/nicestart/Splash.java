1ff5fcb921a61ac16ef8dbb88c7adc2982aa5a62
SpringIsKTho <sergiodela2006@gmail.com>
2025-11-18 13:47:22 +0100

    added splash animation

--- app/src/main/java/com/example/nicestart/Splash.java
+++ app/src/main/java/com/example/nicestart/Splash.java
@@ -1,8 +1,12 @@
 package com.example.nicestart;
 
 import android.content.Intent;
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +20,12 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView wave = findViewById(R.id.logo);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.blink);
+        Animation myanim2 = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        wave.startAnimation(myanim);
+        wave.startAnimation(myanim2);
+
         openApp();
     }
 
